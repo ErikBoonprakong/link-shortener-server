eff3e62afef431a8f880cc6b3d01f4f8a42c2c4f
ErikBoonprakong <e.boonprakong@gmail.com>
2022-07-14 15:36:48 +0100

    changed procfile and npm init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: DENO_ENV=production deno run --allow-net --allow-read --allow-env server.js
+web: DENO_ENV=production deno run --allow-net --allow-read --allow-env --allow-write --unstable server.js
--- server.js
+++ server.js
@@ -1,124 +1,124 @@
-import { Application } from 'https://deno.land/x/abc@v1.3.1/mod.ts'
-import { abcCors } from 'https://deno.land/x/cors/mod.ts'
-import { LocalStorage } from 'https://deno.land/x/storage@0.0.5/mod.ts'
+import { Application } from "https://deno.land/x/abc@v1.3.1/mod.ts";
+import { abcCors } from "https://deno.land/x/cors/mod.ts";
+import { LocalStorage } from "https://deno.land/x/storage@0.0.5/mod.ts";
 
-const store = new LocalStorage('shortenings')
-const counter = new LocalStorage('counter')
-const privateLink = new LocalStorage('private')
-const privateCount = new LocalStorage('privateCount')
+const store = new LocalStorage("shortenings");
+const counter = new LocalStorage("counter");
+const privateLink = new LocalStorage("private");
+const privateCount = new LocalStorage("privateCount");
 const corsConditions = abcCors({
-  origin: 'http://localhost:3000'
-})
+  origin: "http://localhost:3000",
+});
 
-const app = new Application()
-const PORT = 8080
+const app = new Application();
+const PORT = 8080;
 
 function randomFour() {
-  const characters = ['a', 'e', 'r', 'i', 'k', 'o', 'u', '1', '5', '8']
-  let i = 0
-  let str = ''
+  const characters = ["a", "e", "r", "i", "k", "o", "u", "1", "5", "8"];
+  let i = 0;
+  let str = "";
   while (i < 4) {
-    str += characters[Math.floor(Math.random() * characters.length)]
-    i++
+    str += characters[Math.floor(Math.random() * characters.length)];
+    i++;
   }
-  return str
+  return str;
 }
 
 async function validateURL(url) {
-  let valid
+  let valid;
   try {
     if (await fetch(url)) {
-      valid = true
+      valid = true;
     }
   } catch {
-    valid = false
+    valid = false;
   }
-  return valid
+  return valid;
 }
 
 app
   .use(abcCors())
-  .get('/', (server) => {
-    server.json({}, 200)
+  .get("/", (server) => {
+    server.json({}, 200);
   })
-  .get('/urls', (server) => {
-    return server.json(store)
+  .get("/urls", (server) => {
+    return server.json(store);
   })
-  .get('/privateLinks', (server) => {
-    return server.json([privateLink, privateCount])
+  .get("/privateLinks", (server) => {
+    return server.json([privateLink, privateCount]);
   })
-  .get('/visits', (server) => {
-    return server.json(counter)
+  .get("/visits", (server) => {
+    return server.json(counter);
   })
-  .get('/visits/:shortcode', (server) => {
-    const { shortcode } = server.params
-    let currentCount = counter.get(shortcode)
-    return server.json('Visited ' + currentCount + ' times', 200)
+  .get("/visits/:shortcode", (server) => {
+    const { shortcode } = server.params;
+    let currentCount = counter.get(shortcode);
+    return server.json("Visited " + currentCount + " times", 200);
   })
-  .get('/l/:shortcode', async (server) => {
-    const { shortcode } = server.params
+  .get("/l/:shortcode", async (server) => {
+    const { shortcode } = server.params;
     if (shortcode) {
-      console.log(shortcode)
-      const theURL = await store.get(shortcode)
-      const privateURL = await privateLink.get(shortcode)
-      let currentCount = await counter.get(shortcode)
-      let currentCountPrivate = await privateCount.get(shortcode)
-      let valid = await validateURL(theURL)
-      let privateValid = await validateURL(privateURL)
-      console.log(valid)
+      console.log(shortcode);
+      const theURL = await store.get(shortcode);
+      const privateURL = await privateLink.get(shortcode);
+      let currentCount = await counter.get(shortcode);
+      let currentCountPrivate = await privateCount.get(shortcode);
+      let valid = await validateURL(theURL);
+      let privateValid = await validateURL(privateURL);
+      console.log(valid);
 
       if (valid) {
-        counter.set(shortcode, currentCount + 1)
-        server.redirect(theURL)
+        counter.set(shortcode, currentCount + 1);
+        server.redirect(theURL);
       } else if (privateValid) {
-        privateCount.set(shortcode, currentCountPrivate + 1)
-        server.redirect(privateURL)
+        privateCount.set(shortcode, currentCountPrivate + 1);
+        server.redirect(privateURL);
       } else {
-        console.log('URL not valid')
-        return server.json({ Error: 'URL is not valid :(' }, 404)
+        console.log("URL not valid");
+        return server.json({ Error: "URL is not valid :(" }, 404);
       }
     } else {
-      console.log('no url')
-      return server.json({ Error: 'Nottt found' }, 404)
+      console.log("no url");
+      return server.json({ Error: "Nottt found" }, 404);
     }
   })
-  .post('/shortlinks', async (server) => {
-    const body = await server.body
-    const url = body.fullUrl
-    let isPrivate = body.isPrivate
-    let valid = await validateURL(url)
+  .post("/shortlinks", async (server) => {
+    const body = await server.body;
+    const url = body.fullUrl;
+    let isPrivate = body.isPrivate;
+    let valid = await validateURL(url);
     if (valid) {
-      const four = randomFour()
-      console.log(isPrivate)
+      const four = randomFour();
+      console.log(isPrivate);
       if (isPrivate) {
-        await privateLink.set(four, url)
-        await privateCount.set(four, 0)
+        await privateLink.set(four, url);
+        await privateCount.set(four, 0);
       } else {
-        await store.set(four, url)
-        await counter.set(four, 0)
+        await store.set(four, url);
+        await counter.set(four, 0);
       }
-      return server.json({ shortcode: four }, 200)
+      return server.json({ shortcode: four }, 200);
     } else {
-      console.log('bad url :(')
-      return server.json({ Error: 'bad url :(' }, 404)
+      console.log("bad url :(");
+      return server.json({ Error: "bad url :(" }, 404);
     }
   })
-  .delete('/:shortcode', async (server) => {
-    const { shortcode } = server.params
-    const publicCode = await store.get(shortcode)
-    const privateCode = await privateLink.get(shortcode)
+  .delete("/:shortcode", async (server) => {
+    const { shortcode } = server.params;
+    const publicCode = await store.get(shortcode);
+    const privateCode = await privateLink.get(shortcode);
     if (publicCode) {
-      store.delete(shortcode)
-      counter.delete(shortcode)
-      return server.json('Link deleted!', 200)
+      store.delete(shortcode);
+      counter.delete(shortcode);
+      return server.json("Link deleted!", 200);
     } else if (privateCode) {
-      privateLink.delete(shortcode)
-      privateCount.delete(shortcode)
-      return server.json('Private link deleted!', 200)
+      privateLink.delete(shortcode);
+      privateCount.delete(shortcode);
+      return server.json("Private link deleted!", 200);
     } else {
-      return server.json({ Error: 'Invalid short code' }, 400)
+      return server.json({ Error: "Invalid short code" }, 400);
     }
   })
-  .start({ port: PORT })
+  .start({ port: PORT });
 
-console.log(`Server running on http://localhost:${PORT}`)
+console.log(`Server running on http://localhost:${PORT}`);
